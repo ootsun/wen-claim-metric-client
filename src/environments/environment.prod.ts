a4a34c1c926f11773e4468a1f19ac1417db02c73
OoTsun <gabriel.delhaye@hotmail.com>
2022-11-06 15:13:53 +0100

    Use https://wen-claim-metric-server.ootsun-server.ovh/ api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://wen-claim-metric-server.herokuapp.com/',
+  apiBaseUrl: 'https://wen-claim-metric-server.ootsun-server.ovh/',
 };
